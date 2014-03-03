0a488b55e8c0891495cc474bb0084839ad2c9f44
Asi <asisudai@gmail.com>
2014-03-03 11:52:24 -0800

    Adding support for MotionBuilder

--- cgLogging.py
+++ cgLogging.py
@@ -41,25 +41,39 @@ To use:
 """
 
 ## -----------------------------------------------------------------------------
-# Imports
+#   Imports
 ## -----------------------------------------------------------------------------
 import sys
 import logging
 
-## Inside Maya?
+## Load Maya:
 try:
-    import maya
-    _in_maya = True
+	import maya
+	_in_maya = True
 except Exception:
-    _in_maya = False
+	_in_maya = False
 
-## Inside Nuke?
+## Load Nuke:
 try:
-    import nuke
-    _in_nuke = True
+	import nuke
+	_in_nuke = True
 except Exception:
-    _in_nuke = False
+	_in_nuke = False
 
+
+## Load Nuke:
+try:
+	import nuke
+	_in_nuke = True
+except Exception:
+	_in_nuke = False
+
+## Load Mobu:
+try:
+	import pyfbsdk
+	_in_mobu = True
+except Exception:
+	_in_mobu = False
 
 ## -----------------------------------------------------------------------------
 # Globals
@@ -68,7 +82,7 @@ MSG_FORMAT  = "%(asctime)s %(name)s %(levelname)s : %(message)s"
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
-# Levels
+# Levels - same as logging module
 CRITICAL = 50
 FATAL    = CRITICAL
 ERROR    = 40
@@ -81,200 +95,235 @@ NOTSET   = 0
 
 
 ## -----------------------------------------------------------------------------
-# getLogger - Main call to grab a logger instance
+# getLogger - Main Call
 ## -----------------------------------------------------------------------------
-def getLogger( name, shell=True, maya=_in_maya, nuke=_in_nuke, file=None, level=INFO ):
-    '''
-    Get logger - mimicing logging.getLogger() usage.
+def getLogger( name, shell=True, maya=_in_maya, nuke=_in_nuke, mobu=_in_mobu, file=None, level=INFO ):
+	'''
+	Get logger - mimicing the usage of logging.getLogger()
+		name(str) : logger name
+		shell(bol): output to shell
+		maya(bol) : output to maya editor
+		nuke(bol) : output to nuke editor
+		file(str) : output to given filename
+		level(int): logger level
+	'''
+	return Logger( name, shell, maya, nuke, mobu, file, level )
 
-        name(str)  : logger name
-        shell(bol) : output to shell/stdout
-        maya(bol)  : output to maya script editor
-        nuke(bol)  : output to nuke script editor
-        file(str)  : output log into given fullpath-filename
-        level(int) : logger level. default to INFO
-
-    Example:
-        log = getLogger( 'mylogger' )
-        log.info( 'log something' )
-    '''
-    return Logger( name, shell, maya, nuke, file, level )
 
 
 ## -----------------------------------------------------------------------------
 # Logger Class
 ## -----------------------------------------------------------------------------
-class Logger(object):
+class Logger():
+	'''
+	'''
 
-    def __init__( self, name, shell=True, maya=False, nuke=False, file=None, level=INFO ):
+	def __init__(self, name, shell=True, maya=False, nuke=False, mobu=False, file=None, level=INFO ):
+		'''
+		Init Logger
+		'''
 
-        self._name = name
-        self._log  = logging.getLogger(name)
-        self._log.setLevel( level )
+		self.__name   = name
+		self.__logger = logging.getLogger(name)
+		self.__logger.setLevel( level )
 
-        ## SafetyCheck - since logging.getLogger support multi threads, if logger
-        ##               was already created, it should also have the handlers too.
-        ##               so let's avoid creating handlers again, so we won't get
-        ##               double handlers... and make a double eveything.
-        if self._log.handlers:
-            # Logger already have handlers, no need to add more.
-            return
+		## SafetyCheck - Don't create handelr if logger already have some.
+		##               this will prevent double handelrs creation.
+		if self.__logger.handlers:
+			return
 
-        # Format
-        format  = logging.Formatter( MSG_FORMAT, DATE_FORMAT )
+		# Format
+		format  = logging.Formatter( MSG_FORMAT, DATE_FORMAT )
 
-        # Shell:
-        if shell:
-            stream_hdlr = ShellHandler()
-            stream_hdlr.setFormatter( format )
-            self._log.addHandler( stream_hdlr )
+		# Shell:
+		if shell:
+			stream_hdlr = ShellHandler()
+			stream_hdlr.setFormatter( format )
+			self.__logger.addHandler( stream_hdlr )
 
-        # File:
-        if file:
-            file_hdlr = logging.FileHandler(file)
-            file_hdlr.setFormatter( format )
-            self._log.addHandler( file_hdlr )
+		# File:
+		if file:
+			file_hdlr = logging.FileHandler(file)
+			file_hdlr.setFormatter( format )
+			self.__logger.addHandler( file_hdlr )
 
-        # Maya:
-        if maya and _in_maya:
-            maya_hdlr = MayaHandler()
-            maya_hdlr.setFormatter( format )
-            self._log.addHandler( maya_hdlr )
+		# Maya:
+		if maya and _in_maya:
+			maya_hdlr = MayaHandler()
+			maya_hdlr.setFormatter( format )
+			self.__logger.addHandler( maya_hdlr )
 
-        # Nuke:
-        if nuke and _in_nuke:
-            nuke_hdlr = NukeHandler()
-            nuke_hdlr.setFormatter( format )
-            self._log.addHandler( nuke_hdlr )
+		# Nuke:
+		if nuke and _in_nuke:
+			nuke_hdlr = NukeHandler()
+			nuke_hdlr.setFormatter( format )
+			self.__logger.addHandler( nuke_hdlr )
 
-    def __getattr__(self, attr):
-        '''
-        expose builtin logger attributes.
-        this won't be needed if I could inherit the logger directly.
-        But the logging.getLoggerClass() don't seem to work without jumping
-        through to many hoops to make it worth it.
-        '''
-        if hasattr( self._log, attr ):
-            return getattr( self._log, attr )
-        else:
-            raise AttributeError, "No attribute %s" %attr
+		# Mobu:
+		if mobu and _in_mobu:
+			mobu_hdlr = MobuHandler()
+			mobu_hdlr.setFormatter( format )
+			self.__logger.addHandler( mobu_hdlr )
 
-    def __repr__(self):
-        return "%s(%s Level:%i)" % ( self.__class__, self._name, self.level )
+	def __repr__(self):
+		'''
+		string representation.
+		'''
+		return "%s(%s Level:%i)" % ( self.__class__, self.__name, self.level )
 
+	def __getattr__(self, attr):
+		'''
+		Use logging.Logger attributes.
+		'''
+		if hasattr( self.__logger, attr ):
+			return getattr( self.__logger, attr )
+		else:
+			raise AttributeError, "No attribute %s" %attr
 
+	## LEVELS
+	def debug(self, msg):
+		self.__logger.debug(msg)
 
-    ## LEVELS
-    def debug(self, msg):
-        self._log.debug(msg)
+	def info(self, msg):
+		self.__logger.info(msg)
 
-    def info(self, msg):
-        self._log.info(msg)
+	def warning(self, msg):
+		self.__logger.warning(msg)
 
-    def warning(self, msg):
-        self._log.warning(msg)
+	def fatal(self, msg):
+		self.__logger.fatal(msg)
 
-    def error(self, msg):
-        self._log.error(msg)
-
-    def fatal(self, msg):
-        self._log.fatal(msg)
-
-    def critical(self, msg):
-        self._log.critical(msg)
+	def critical(self, msg):
+		self.__logger.critical(msg)
 
 
 ## -----------------------------------------------------------------------------
 # ShellHandler
 ## -----------------------------------------------------------------------------
 class ShellHandler(logging.Handler):
-    '''
-    Shell Handler - emits logs to stdout.
-    we'll use sys.__stdout__ to bypass maya and nuke script-editors
-    so they won't show double logging.
-    '''
+	'''
+	Shell Handler - emits logs to shell only.
+	by passing maya and nuke editors by using sys.__stdout__
+	'''
 
-    def __init__(self):
-        logging.Handler.__init__(self)
+	def __init__(self):
+		logging.Handler.__init__(self)
 
-    def emit(self, record):
-        sys.__stdout__.write( "%s\n" %self.format(record) )
+	def emit(self, record):
+
+		try:
+			sys.__stdout__.write( "%s\n" %self.format(record) )
+		except IOError:
+			## MotionBuilder is doing something funky with python,
+			## so sometimes ( not always ) is blocked from writting:
+			sys.stdout.write( "%s\n" %self.format(record) )
 
 
 ## -----------------------------------------------------------------------------
 # MayaHandler
 ## -----------------------------------------------------------------------------
 class MayaHandler(logging.Handler):
-    '''
-    Maya specific handler - will emit warnings using nuke.warning()
-    and critical and fatal levels will popup a warning dialog so artists
-    won't miss important errors.
-    '''
-    def __init__(self):
-        logging.Handler.__init__(self)
+	'''
+	Maya Handler - emits logs into maya's script editor.
+	warning will emit maya.cmds.warning()
+	critical and fatal would popup msg dialog to alert of the error.
+	'''
+	def __init__(self):
+		logging.Handler.__init__(self)
 
-    def emit(self, record):
+	def emit(self, record):
 
-        # Formated message:
-        msg = self.format(record)
+		# Formated message:
+		msg = self.format(record)
 
-        if record.funcName == "warning":
-            maya.cmds.warning( "\n"+msg )
+		if record.funcName == "warning":
+			maya.cmds.warning( "\n"+msg )
 
-        elif record.funcName in [ "critical", "fatal" ]:
+		elif record.funcName in [ "critical", "fatal" ]:
 
-            ## Emit stdout print:
-            sys.stdout.write("\n"+msg+"\n")
+			## Emit stdout print:
+			sys.stdout.write("\n"+msg+"\n")
 
-            ## Open dialog if not in batch mode:
-            if maya.cmds.about( batch=True ) == False:
+			## Open dialog if not in batch mode:
+			if maya.cmds.about( batch=True ) == False:
 
-                maya.cmds.confirmDialog(title   = "A %s have accure" %record.funcName,
-                                        message = record.message,
-                                        button  = ['Dismiss'],
-                                        messageAlign = "left")
+				maya.cmds.confirmDialog(title   = "A %s have accure" %record.funcName,
+										message = record.message,
+										button  = ['Dismiss'],
+										messageAlign = "left")
 
-        else:
-            sys.stdout.write(msg+"\n")
+		else:
+			sys.stdout.write(msg+"\n")
 
 
 ## -----------------------------------------------------------------------------
 # NukeHandler
 ## -----------------------------------------------------------------------------
 class NukeHandler(logging.Handler):
-    '''
-    Nuke specific handler - will emit warnings using nuke.warning()
-    and critical and fatal levels will popup a warning dialog so artists
-    won't miss important errors.
-    '''
-    def __init__(self):
-        logging.Handler.__init__(self)
+	'''
+	Nuke Handler - emits logs into nuke's script editor.
+	warning will emit nuke.warning()
+	critical and fatal would popup msg dialog to alert of the error.
+	'''
+	def __init__(self):
+		logging.Handler.__init__(self)
 
-    def emit(self, record):
+	def emit(self, record):
 
-        # Formated message:
-        msg = self.format(record)
+		# Formated message:
+		msg = self.format(record)
 
-        if record.funcName == "warning":
-            nuke.warning(msg)
+		if record.funcName == "warning":
+			nuke.warning(msg)
 
-        elif record.funcName in [ "critical", "fatal" ]:
-            nuke.error(msg)
-            nuke.message(record.message)
+		elif record.funcName in [ "critical", "fatal" ]:
+			nuke.error(msg)
+			nuke.message(record.message)
 
-        else:
-            sys.stdout.write(msg)
+		else:
+			sys.stdout.write(msg)
+
+## -----------------------------------------------------------------------------
+# MobuHandler
+## -----------------------------------------------------------------------------
+class MobuHandler(logging.Handler):
+	'''
+	MotionBuilder Handler - emits logs into motionbuilder's script editor.
+	'''
+	def __init__(self):
+		logging.Handler.__init__(self)
+
+	def emit(self, record):
+
+		# Formated message:
+		msg = self.format(record)
+
+		if record.funcName == "warning":
+			sys.stdout.write(msg+"\n")
+
+		elif record.funcName == "error":
+			sys.stderr.write(msg+"\n")
+
+		elif record.funcName in [ "critical", "fatal" ]:
+			FBMessageBox( record.funcName, msg, "OK" )
+			sys.stderr.write(msg+"\n")
+
+		else:
+			sys.stdout.write(msg+"\n")
 
 
 ## -----------------------------------------------------------------------------
-# _name__
+# __name__
 ## -----------------------------------------------------------------------------
 if __name__ == '__main__':
-    ## Test code:
 
-    log = getLogger( "logger_name", shell=True )
-    log.setLevel( logging.DEBUG )
-    log.debug('debug')
-    log.info('info')
-    log.warning('warning')
-    log.fatal('fatal')
+	log = getLogger( "logger_name", shell=True )
+	log.setLevel( logging.DEBUG )
+	log.debug('debug msg')
+	log.info('info msg')
+	log.warning('warning msg')
+	log.fatal('fatal msg')
+
+
+
+
